9ee88f8c9b8482bb5555a9cfde3b0cca4f502f6e
DeveloperMohit1 <mohituagrawal@gmail.com>
2021-11-12 16:38:58 +0530

    first commit

---
+++ chatbot.py
@@ -0,0 +1,41 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from flask import Flask, render_template, request
+
+# Creating ChatBot Instance
+chatbot = ChatBot('PharmaBot')
+
+ # Training with Personal Ques & Ans 
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
+
+# Training with English Corpus Data 
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+trainer_corpus.train(
+    'chatterbot.corpus.english'
+) 
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(chatbot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run() 
